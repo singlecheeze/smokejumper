bbb3b7913e3f5b0134e4c503140302295bfd0d01
singlecheeze <byte.light.77@gmail.com>
2024-10-18 10:37:24 -0400

    sync git

--- fc/osnexus/create_luns.py
+++ fc/osnexus/create_luns.py
@@ -14,7 +14,7 @@ from quantastor.qs_client import QuantastorClient
 from fc.osnexus.utils import run_in_executor
 from utils.timer import Timer
 
-NUM_LUNS = 300
+NUM_LUNS = 150
 LUN_NAME_PREFIX = "API.LUN"
 QUANTASTOR_IP = "172.16.1.119"
 QUANTASTOR_USER = "admin"
@@ -115,76 +115,76 @@ async def main():
     # print(json.dumps(get_luns(client, verbose=True), sort_keys=True, indent=4, separators=(',', ': ')))
 
 
-    # Run operations in a debug pod on OCP
-    oc_context = Context()
-    oc_context.set_default_skip_tls_verify = True
-    oc_context.oc_path = f"{THIS_FILE_DIR}\\oc.exe"
-    oc_context.kubeconfig_path = f"{THIS_FILE_DIR}\\kubeconfig"
-    oc_context.token = "sha256~osw2Pge9NdDJdAKvej3rn6rsu8zJBkHs1M_nFZqX7d4"
-    oc_context.api_server = "https://api.ocp.sno.localdomain:6443"
-
-    with oc.timeout(60 * 30), oc.tracking() as tracker, oc_context:
-        if oc.get_config_context() is None:
-            print(f'Current context not set! Logging into API server: {oc_context.api_server}')
-            try:
-                oc.invoke('login')
-            except OpenShiftPythonException:
-                print('error occurred logging into API Server')
-                traceback.print_exc()
-                print(f'Tracking:\n{tracker.get_result().as_json(redact_streams=False)}\n\n')
-                exit(1)
-
-        print(f'Current oc context: {oc.get_config_context().strip()}')
-
-        try:
-            # print(f"Found: {len(oc.selector('pods').objects())} pods")
-            # print(f"Current project: {oc.get_project_name()}")
-
-            get_scsi_hosts_out, get_scsi_hosts_err = debug_pod_exec(
-                operation="get_scsi_hosts",
-                node_name=TEST_HOST,
-                stdin_str="ls /sys/class/scsi_host | grep host")
-
-            if get_scsi_hosts_out:
-                # print(get_scsi_hosts_out)
-                for host_id in get_scsi_hosts_out.splitlines():
-                    rescan_out, rescan_err = debug_pod_exec(
-                        operation=f"recan_{host_id}",
-                        node_name=TEST_HOST,
-                        stdin_str=f"(time echo '- - -' > /sys/class/scsi_host/{host_id}/scan) 2> >( tee /dev/stderr ) | grep real | awk '{{print $2}}'")
-                    print(f"Rescanned {host_id} in: {rescan_out}")
-
-            ls_disk_by_path_out, ls_disk_by_path_err = debug_pod_exec(
-                operation=f"ls_disk_by_path",
-                node_name=TEST_HOST,
-                stdin_str="ls /host/dev/disk/by-path/")
-
-            ls_disk_by_path_out = ls_disk_by_path_out.splitlines()
-
-            def parse_lun_list(lun_num: str):
-                wwn = "100000109bc3914e-0x21000024ff1e85b6"
-
-                test_lun_name = "fc-0x" + wwn + "-lun-" + lun_num
-                print(f"Testing contains for {test_lun_name}:")
-                for lun in ls_disk_by_path_out:
-                    if test_lun_name in lun:
-                        print(lun)
-                print()
-
-                test_lun_regex = "^(pci-.*-fc|fc)-0x" + wwn + "-lun-" + lun_num + "$"
-                print(f"Testing REGEX expression {test_lun_regex}:")
-                for lun in ls_disk_by_path_out:
-                    x = re.search(test_lun_regex, lun)
-                    if x is not None:
-                        print(x.group())
-                print()
-
-            parse_lun_list("1")
-            parse_lun_list("11")
-
-        except OpenShiftPythonException:
-            print('Error occurred getting pods')
-            traceback.print_exc()
-            print(f'Tracking:\n{tracker.get_result().as_json(redact_streams=False)}\n\n')
+    # # Run operations in a debug pod on OCP
+    # oc_context = Context()
+    # oc_context.set_default_skip_tls_verify = True
+    # oc_context.oc_path = f"{THIS_FILE_DIR}\\oc.exe"
+    # oc_context.kubeconfig_path = f"{THIS_FILE_DIR}\\kubeconfig"
+    # oc_context.token = "sha256~osw2Pge9NdDJdAKvej3rn6rsu8zJBkHs1M_nFZqX7d4"
+    # oc_context.api_server = "https://api.ocp.sno.localdomain:6443"
+    #
+    # with oc.timeout(60 * 30), oc.tracking() as tracker, oc_context:
+    #     if oc.get_config_context() is None:
+    #         print(f'Current context not set! Logging into API server: {oc_context.api_server}')
+    #         try:
+    #             oc.invoke('login')
+    #         except OpenShiftPythonException:
+    #             print('error occurred logging into API Server')
+    #             traceback.print_exc()
+    #             print(f'Tracking:\n{tracker.get_result().as_json(redact_streams=False)}\n\n')
+    #             exit(1)
+    #
+    #     print(f'Current oc context: {oc.get_config_context().strip()}')
+    #
+    #     try:
+    #         # print(f"Found: {len(oc.selector('pods').objects())} pods")
+    #         # print(f"Current project: {oc.get_project_name()}")
+    #
+    #         get_scsi_hosts_out, get_scsi_hosts_err = debug_pod_exec(
+    #             operation="get_scsi_hosts",
+    #             node_name=TEST_HOST,
+    #             stdin_str="ls /sys/class/scsi_host | grep host")
+    #
+    #         if get_scsi_hosts_out:
+    #             # print(get_scsi_hosts_out)
+    #             for host_id in get_scsi_hosts_out.splitlines():
+    #                 rescan_out, rescan_err = debug_pod_exec(
+    #                     operation=f"recan_{host_id}",
+    #                     node_name=TEST_HOST,
+    #                     stdin_str=f"(time echo '- - -' > /sys/class/scsi_host/{host_id}/scan) 2> >( tee /dev/stderr ) | grep real | awk '{{print $2}}'")
+    #                 print(f"Rescanned {host_id} in: {rescan_out}")
+    #
+    #         ls_disk_by_path_out, ls_disk_by_path_err = debug_pod_exec(
+    #             operation=f"ls_disk_by_path",
+    #             node_name=TEST_HOST,
+    #             stdin_str="ls /host/dev/disk/by-path/")
+    #
+    #         ls_disk_by_path_out = ls_disk_by_path_out.splitlines()
+    #
+    #         def parse_lun_list(lun_num: str):
+    #             wwn = "100000109bc3914e-0x21000024ff1e85b6"
+    #
+    #             test_lun_name = "fc-0x" + wwn + "-lun-" + lun_num
+    #             print(f"Testing contains for {test_lun_name}:")
+    #             for lun in ls_disk_by_path_out:
+    #                 if test_lun_name in lun:
+    #                     print(lun)
+    #             print()
+    #
+    #             test_lun_regex = "^(pci-.*-fc|fc)-0x" + wwn + "-lun-" + lun_num + "$"
+    #             print(f"Testing REGEX expression {test_lun_regex}:")
+    #             for lun in ls_disk_by_path_out:
+    #                 x = re.search(test_lun_regex, lun)
+    #                 if x is not None:
+    #                     print(x.group())
+    #             print()
+    #
+    #         parse_lun_list("1")
+    #         parse_lun_list("11")
+    #
+    #     except OpenShiftPythonException:
+    #         print('Error occurred getting pods')
+    #         traceback.print_exc()
+    #         print(f'Tracking:\n{tracker.get_result().as_json(redact_streams=False)}\n\n')
 if __name__ == "__main__":
     asyncio.run(main())
